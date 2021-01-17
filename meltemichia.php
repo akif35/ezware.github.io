1a06f3e166ef6947fd7bd14f18e749ef2acdaf65
akif35 <50207862+akif35@users.noreply.github.com>
2021-01-17 18:15:48 +0300

    Add files via upload

---
+++ meltemichia.php
@@ -0,0 +1,14 @@
+<?php
+    function RandomString()
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randstring = '';
+        for ($i = 0; $i < 10; $i++) {
+            $randstring = $characters[rand(0, strlen($characters))];
+        }
+        return $randstring;
+    }
+
+    RandomString();
+	echo ("Beni her yenilediğinde sana özel yeni bir kod üretirim")
+    echo $randstring;
